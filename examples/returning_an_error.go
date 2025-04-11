766e77e88af39c4c74b02aa3c8ff5680d7df6e44
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-04-11 14:39:37 +0200

    Documenting returns under error conditions

--- examples/returning_an_error.go
+++ examples/returning_an_error.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+// Concat concatenates a bunch of strings, separated by spaces.
+// It returns an empty string and an error if no strings were passed in.
 func Concat(parts ...string) (string, error) {
 	if len(parts) == 0 {
 		return "", errors.New("no strings supplied")
