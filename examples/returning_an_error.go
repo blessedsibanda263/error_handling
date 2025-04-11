560941502c060dd94de763a7e9eb469896ef84d2
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-04-11 14:15:38 +0200

    Return and handling an error

---
+++ examples/returning_an_error.go
@@ -0,0 +1,24 @@
+package examples
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Concat(parts ...string) (string, error) {
+	if len(parts) == 0 {
+		return "", errors.New("no strings supplied")
+	}
+	return strings.Join(parts, " "), nil
+}
+
+func ReturningAnError() {
+	args := os.Args[1:]
+	if result, err := Concat(args...); err != nil {
+		fmt.Printf("Error: %s\n", err)
+	} else {
+		fmt.Printf("Concatenated string: '%s'\n", result)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "error_handling/examples"
+
+func main() {
+	examples.ReturningAnError()
+}
