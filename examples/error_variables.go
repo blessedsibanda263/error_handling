c4d08e12e1ebbc5598fbdc6c0216cbc1cf005cd0
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-04-11 14:52:31 +0200

    Handling two different errors

---
+++ examples/error_variables.go
@@ -0,0 +1,48 @@
+package examples
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const MAX_TIMEOUTS = 5
+
+var ErrTimeout = errors.New("The request timed out")
+var ErrRejected = errors.New("The request was rejected")
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func sendRequest() (string, error) {
+	switch rand.Intn(3) % 3 {
+	case 0:
+		return "Success", nil
+	case 1:
+		return "", ErrRejected
+	default:
+		return "", ErrTimeout
+	}
+}
+
+func ErrorVariables() {
+	response, err := sendRequest()
+	if errors.Is(err, ErrTimeout) {
+		timeouts := 0
+		for err.Error() == ErrTimeout.Error() {
+			timeouts++
+			fmt.Println("Timeout. Retrying.")
+			if timeouts == MAX_TIMEOUTS {
+				panic("too many timeouts")
+			}
+			response, err = sendRequest()
+		}
+	}
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(response)
+	}
+}
